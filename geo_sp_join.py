2017482652d6464476d25f79da786d27a09e6eb4
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2019-12-23 08:17:53 +0000

    prova1

---
+++ geo_sp_join.py
@@ -0,0 +1,37 @@
+
+import geopandas as gpd
+import pandas as pd
+
+
+
+lombardia = gpd.read_file('R03_11_WGS84.shp')
+print(lombardia.head())
+
+cened2 = pd.read_csv('res.csv')
+print(cened2.head())
+
+from shapely.geometry import Point 
+geometry = [Point(xy) for xy in zip(cened2['lat'], cened2['lng'])]
+
+Cened2crs = {'init': 'epsg:4326'}
+
+cened2GDF = gpd.GeoDataFrame(cened2, crs=Cened2crs, geometry=geometry)
+print(cened2GDF.head())
+
+#Convertiamo ora le coordinate dei punti nello stesso sistema di riferimento del dataframe della lombardia. 
+cened2GDF = cened2GDF.to_crs(lombardia.crs) 
+cened2GDF.head()
+
+#Per poter effettuare la sjoin (cioe' per associare ad un punto - cioe' ad un indirizzo
+# - il poligono in cui si trova - cioe' la sezione del censimento in cui si trova) 
+#dobbiamo assicurarci che il campo geometry del primo geodataframe sia di tipo POINT 
+#mentre quello del secondo geodataframe sia di tipo POLYGON
+
+print(type(cened2GDF.geometry[0])) #Verifico i dati del cened (Point)
+
+print(type(lombardia.geometry[0])) #Verifico i dati istat (Polygon)
+
+#Effettuiamo ora la sjoin
+indirizzoSezione = gpd.sjoin(left_df=cened2GDF, right_df=lombardia, how="left", op="intersects")
+
+print(indirizzoSezione.head())
---
+++ setup.py
@@ -0,0 +1,6 @@
+import os
+os.chdir("/spatialindex-src-1.8.5")
+!./configure
+!make
+!make install
+!ldconfig
